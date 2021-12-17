e6e7283353df73f19b777d5d2cddf52a676a7ae5
ebolzan <ebolzan@inf.ufsm.br>
2021-12-16 22:17:56 -0300

    refactor(student): generate getter and setter for gradeFinal field

--- src/models/Student.java
+++ src/models/Student.java
@@ -4,13 +4,23 @@ import java.util.Date;
 
 public class Student extends Person {
 
-    private final Float finalGrade;
+    private Float finalGrade;
 
     public Student(String name, String phone, Date birthDate, Date insertDate, Date lastChangeDate, Float finalGrade) {
 
         super( name,  phone,  birthDate,  insertDate, lastChangeDate);
         this.finalGrade = finalGrade;
     }
+
+    public Float getFinalGrade() {
+        return finalGrade;
+    }
+
+    public void setFinalGrade(Float grade){
+        this.finalGrade = grade;
+    }
+
+
 
     @Override
     public String toString() {
