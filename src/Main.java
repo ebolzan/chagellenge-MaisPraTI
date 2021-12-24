ad702da61b66769b568a3682cdc6da2e226ee4e6
ebolzan <ebolzan@inf.ufsm.br>
2021-12-24 16:41:57 -0300

    refactor(main): change structured program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import controllers.PersonController;
 import models.DaoPerson;
 import models.Person;
 import models.Student;
+import views.TerminalView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +15,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        TerminalView t = new TerminalView();
+        DaoPerson d = new DaoPerson();
+
+        PersonController personController = new PersonController(d,t);
+
+        personController.init();
+
+        System.exit(0);
+
+
         Scanner in = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         DaoPerson daoPerson = new DaoPerson();
