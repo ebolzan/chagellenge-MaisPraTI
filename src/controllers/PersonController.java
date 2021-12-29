6989fa02fc4fcc905249437662ed677e23db2d52
ebolzan <ebolzan@inf.ufsm.br>
2021-12-29 01:01:15 -0300

    refactor(personcontroller): set interval note

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -53,6 +53,10 @@ public class PersonController {
     public boolean insertPerson(String name, String phone, Date date, Date dateInserted, Date dateInsertModified, Float grade)
     {
         Student student = new Student(name,phone,date, dateInserted, dateInsertModified,grade);
+
+        if (student.getFinalGrade() == null)
+            return false;
+
         return this.daoPerson.insertPerson(student);
     }
 
@@ -111,9 +115,16 @@ public class PersonController {
                 Float grade = Float.parseFloat(newValue);
                 Person student = this.daoPerson.getPerson(indexPerson);
                 if(student instanceof Student){
-                    ((Student)student).setFinalGrade(grade);
-                    updateLastchange(indexPerson);
-                    output = true;
+                    try {
+                        ((Student)student).setFinalGrade(grade);
+                        updateLastchange(indexPerson);
+                        output = true;
+                    }
+                    catch (Exception e)
+                    {
+                        e.getMessage();
+                        output = false;
+                    }
                 }
                 break;
         }
